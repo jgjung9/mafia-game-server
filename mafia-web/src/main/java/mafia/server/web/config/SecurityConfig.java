2d775b2ff996e7c321b43ce6a63b9773383ec708
gio <wnsgyo9@gmail.com>
2025-09-25 06:25:48 +0900

    chore(web): h2 console 관련 설정 추가

--- mafia-web/src/main/java/mafia/server/web/config/SecurityConfig.java
+++ mafia-web/src/main/java/mafia/server/web/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package mafia.server.web.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
@@ -16,8 +21,16 @@ public class SecurityConfig {
                         .requestMatchers("/auth/**", "/webapp/**", "/css/**", "/js/**", "/favicon.*").permitAll()
                         .requestMatchers("/rpc").authenticated()
                 )
+                .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
         ;
         return http.build();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "spring.h2.console.enabled", havingValue = "true")
+    public WebSecurityCustomizer configureH2ConsoleEnable() {
+        return web -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console());
     }
 }
