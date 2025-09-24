2cd927e1fa59816daa3c8e63c6ebb4efaa483e63
gio <wnsgyo9@gmail.com>
2025-09-25 05:13:29 +0900

    chore(web): 기본적인 시큐리티 Configuration 작성

--- mafia-web/src/main/java/mafia/server/web/config/MafiaWebConfig.java
+++ mafia-web/src/main/java/mafia/server/web/config/MafiaWebConfig.java
@@ -1,8 +1,15 @@
 package mafia.server.web.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class MafiaWebConfig {
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
