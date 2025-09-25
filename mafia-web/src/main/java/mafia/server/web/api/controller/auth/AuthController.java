8e6c6681dec6765b7bee8d256371b4cb5e38f26d
gio <wnsgyo9@gmail.com>
2025-09-26 01:25:23 +0900

    feat(web): 로그인 기능 구현

--- mafia-web/src/main/java/mafia/server/web/api/controller/auth/AuthController.java
+++ mafia-web/src/main/java/mafia/server/web/api/controller/auth/AuthController.java
@@ -2,16 +2,25 @@ package mafia.server.web.api.controller.auth;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import mafia.server.web.api.controller.ApiResponse;
+import mafia.server.web.api.controller.auth.request.LoginRequest;
 import mafia.server.web.api.controller.auth.request.SignupRequest;
+import mafia.server.web.api.controller.auth.response.LoginResponse;
+import mafia.server.web.auth.AccountDetails;
+import mafia.server.web.auth.provider.JwtProvider;
 import mafia.server.web.common.annotation.ExecutionTime;
 import mafia.server.web.service.account.AccountService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @ExecutionTime
 @RestController
 @RequiredArgsConstructor
@@ -19,11 +28,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AccountService accountService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtProvider jwtProvider;
 
     @PostMapping("/signup")
     public ResponseEntity<ApiResponse<Void>> signup(@Valid @RequestBody SignupRequest request) {
         accountService.createAccount(request.username(), request.password());
         return ResponseEntity.created(null)
                 .body(ApiResponse.created());
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<LoginResponse>> login(@Valid @RequestBody LoginRequest request) {
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.username(), request.password()));
+        AccountDetails accountDetails = (AccountDetails) authenticate.getPrincipal();
+        LoginResponse response = new LoginResponse(
+                jwtProvider.generateToken(accountDetails), jwtProvider.generateRefreshToken(accountDetails),
+                jwtProvider.getTokenType(), jwtProvider.getExpirationTime()
+        );
+        log.debug("loginResponse={}", response);
+        return ResponseEntity.ok(ApiResponse.ok(response));
     }
 }
--- mafia-web/src/test/java/mafia/server/web/api/controller/auth/AuthControllerTest.java
+++ mafia-web/src/test/java/mafia/server/web/api/controller/auth/AuthControllerTest.java
@@ -1,7 +1,11 @@
 package mafia.server.web.api.controller.auth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import mafia.server.web.api.controller.auth.request.LoginRequest;
 import mafia.server.web.api.controller.auth.request.SignupRequest;
+import mafia.server.web.auth.AccountDetails;
+import mafia.server.web.auth.AccountDto;
+import mafia.server.web.auth.provider.JwtProvider;
 import mafia.server.web.service.account.AccountService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,12 +13,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static org.mockito.BDDMockito.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -28,10 +41,12 @@ class AuthControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
-
     @MockitoBean
     private AccountService accountService;
-
+    @MockitoBean
+    private AuthenticationManager authenticationManager;
+    @MockitoBean
+    private JwtProvider jwtProvider;
 
     @TestConfiguration
     static class TestSecurityConfiguration {
@@ -137,4 +152,109 @@ class AuthControllerTest {
                 .andExpect(jsonPath("$.status").value("400"))
         ;
     }
+
+    @Test
+    @DisplayName("정상적으로 로그인에 성공한다")
+    void login() throws Exception {
+        // given
+        given(authenticationManager.authenticate(any())).willReturn(createTestingAuthentication());
+        given(jwtProvider.generateToken(any())).willReturn("testToken");
+        given(jwtProvider.generateRefreshToken(any())).willReturn("testToken");
+        given(jwtProvider.getTokenType()).willReturn("Bearer");
+        given(jwtProvider.getExpirationTime()).willReturn(3600L);
+        LoginRequest request = new LoginRequest("test1234", "password123");
+
+        // when & then
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(request))
+                        .accept(MediaType.APPLICATION_JSON_VALUE)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.body.accessToken").value("testToken"))
+                .andExpect(jsonPath("$.body.refreshToken").value("testToken"))
+                .andExpect(jsonPath("$.body.tokenType").value("Bearer"))
+                .andExpect(jsonPath("$.body.expiresIn").value(3600))
+        ;
+    }
+
+    @Test
+    @DisplayName("로그인 요청 아이디 길이는 8자리 이상이어야 한다")
+    void login_shouldBeLoginUsernameLengthGreaterThanEqual8() throws Exception {
+        // given
+        LoginRequest request = new LoginRequest("test12", "password123");
+
+        // when & then
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(request))
+                        .accept(MediaType.APPLICATION_JSON_VALUE)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value("400"))
+        ;
+    }
+
+    @Test
+    @DisplayName("로그인 요청 아이디 길이는 16자리 이하여야 한다")
+    void login_shouldBeLoginUsernameLengthLessThanEqual16() throws Exception {
+        // given
+        LoginRequest request = new LoginRequest("test1234longlonglong", "password123");
+
+        // when & then
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(request))
+                        .accept(MediaType.APPLICATION_JSON_VALUE)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value("400"))
+        ;
+    }
+
+    @Test
+    @DisplayName("로그인 요청 비밀번호 길이는 8자리 이상이어야 한다")
+    void login_shouldBeLoginPasswordLengthGreaterThanEqual8() throws Exception {
+        // given
+        LoginRequest request = new LoginRequest("test1234", "short1");
+
+        // when & then
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(request))
+                        .accept(MediaType.APPLICATION_JSON_VALUE)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value("400"))
+        ;
+    }
+
+    @Test
+    @DisplayName("로그인 요청 비밀번호 길이는 16자리 이하여야 한다")
+    void login_shouldBeLoginPasswordLengthLessThanEqual16() throws Exception {
+        // given
+        LoginRequest request = new LoginRequest("test1234", "password123longlonglong");
+
+        // when & then
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(request))
+                        .accept(MediaType.APPLICATION_JSON_VALUE)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value("400"))
+        ;
+    }
+
+    private Authentication createTestingAuthentication() {
+        AccountDto accountDto = new AccountDto(1L, "test1234", "password123");
+        AccountDetails accountDetails = new AccountDetails(accountDto, List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        return new TestingAuthenticationToken(accountDetails, null, accountDetails.getAuthorities());
+    }
+
 }
