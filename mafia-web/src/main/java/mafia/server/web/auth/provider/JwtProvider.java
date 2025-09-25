a4ed22d4dcc8badf5687f0dbecdc6a6a1b350871
gio <wnsgyo9@gmail.com>
2025-09-26 00:35:48 +0900

    feat(web): jwt provider 추가

---
+++ mafia-web/src/main/java/mafia/server/web/auth/provider/JwtProvider.java
@@ -0,0 +1,95 @@
+package mafia.server.web.auth.provider;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import mafia.server.web.auth.AccountContext;
+import mafia.server.web.auth.AccountDetails;
+import mafia.server.web.auth.AccountDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.List;
+
+@Slf4j
+@Component
+public class JwtProvider {
+
+    private final SecretKey JWT_SECRET_KEY;
+    private final long EXPIRATION_TIME;
+    private final long REFRESH_EXPIRATION_TIME;
+
+    public JwtProvider(
+            @Value("${jwt.secret_key}") String jwtSecretKey,
+            @Value("${jwt.expiration_time:3600}") long expirationTime,
+            @Value("${jwt.refresh_expiration_time:1209600}") long refreshExpirationTime
+    ) {
+        JWT_SECRET_KEY = Keys.hmacShaKeyFor(jwtSecretKey.getBytes(StandardCharsets.UTF_8));
+        EXPIRATION_TIME = expirationTime;
+        REFRESH_EXPIRATION_TIME = refreshExpirationTime;
+    }
+
+    public String generateToken(AccountDetails accountDetails) {
+        return generateToken(accountDetails, false);
+    }
+
+    public String generateRefreshToken(AccountDetails accountDetails) {
+        return generateToken(accountDetails, true);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            getClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException exception) {
+            return false;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public AccountContext getAccountContextFromToken(String token) {
+        if (!validateToken(token)) {
+            throw new IllegalArgumentException("Invalid Token: " + token);
+        }
+        Claims claims = getClaims(token);
+        return new AccountContext(
+                claims.get("accountId", Long.class),
+                claims.get("authorities", List.class).stream()
+                        .map(auth -> new SimpleGrantedAuthority(auth.toString()))
+                        .toList()
+        );
+    }
+
+    private String generateToken(AccountDetails accountDetails, boolean isRefresh) {
+        AccountDto accountDto = accountDetails.getAccountDto();
+        return Jwts.builder()
+                .subject(String.valueOf(accountDto.id()))
+                .claim("accountId", accountDto.id())
+                .claim(
+                        "authorities", accountDetails.getAuthorities().stream()
+                                .map(GrantedAuthority::getAuthority).toList()
+                )
+                .signWith(JWT_SECRET_KEY)
+                .expiration(createExpiration(isRefresh ? REFRESH_EXPIRATION_TIME : EXPIRATION_TIME))
+                .compact();
+    }
+
+    private Date createExpiration(long seconds) {
+        return new Date(System.currentTimeMillis() + (seconds * 1000));
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(JWT_SECRET_KEY)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
---
+++ mafia-web/src/test/java/mafia/server/web/auth/provider/JwtProviderTest.java
@@ -0,0 +1,110 @@
+package mafia.server.web.auth.provider;
+
+import mafia.server.web.auth.AccountContext;
+import mafia.server.web.auth.AccountDetails;
+import mafia.server.web.auth.AccountDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class JwtProviderTest {
+
+    private final String jwtSecretKey = "d606a53ae647793f00d9e72598d28457dc9c51ad1f84e33dcb74233531515b11";
+    private final long expirationTime = 1L;
+    private final long refreshExpirationTime = 1L;
+    private JwtProvider jwtProvider = new JwtProvider(jwtSecretKey, expirationTime, refreshExpirationTime);
+
+    @Test
+    @DisplayName("토큰을 생성한다")
+    void generateToken() throws Exception {
+        // given
+        AccountDetails accountDetails = createAccountDetails(1L, "testname1");
+
+        // when
+        String token = jwtProvider.generateToken(accountDetails);
+        String refreshToken = jwtProvider.generateRefreshToken(accountDetails);
+
+        // then
+        assertThat(token).isNotEmpty();
+        assertThat(token).hasSizeGreaterThan(128);
+
+        assertThat(refreshToken).isNotEmpty();
+        assertThat(refreshToken).hasSizeGreaterThan(128);
+    }
+
+    @Test
+    @DisplayName("생성된 토큰을 검증할 수 있다")
+    void validateToken() throws Exception {
+        // given
+        AccountDetails accountDetails = createAccountDetails(1L, "testname1");
+        String token = jwtProvider.generateToken(accountDetails);
+        String refreshToken = jwtProvider.generateRefreshToken(accountDetails);
+
+        // when & then
+        assertThat(jwtProvider.validateToken(token)).isTrue();
+        assertThat(jwtProvider.validateToken(refreshToken)).isTrue();
+    }
+
+    @Test
+    @DisplayName("만료된 토큰은 검증할 수 있다")
+    void validateToken_tokenWhenExpired() throws Exception {
+        // given
+        AccountDetails accountDetails = createAccountDetails(1L, "testname1");
+        String token = jwtProvider.generateToken(accountDetails);
+        String refreshToken = jwtProvider.generateRefreshToken(accountDetails);
+
+        // when
+        Thread.sleep(expirationTime * 1000);
+
+        // then
+        assertThat(jwtProvider.validateToken(token)).isFalse();
+        assertThat(jwtProvider.validateToken(refreshToken)).isFalse();
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 토큰을 검증할 수 있다")
+    void validateToken_whenInvalidValue() throws Exception {
+        // given
+        String token = "invalidvalidfioewfsdfdfsf";
+
+        // when & then
+        assertThat(jwtProvider.validateToken(token)).isFalse();
+    }
+
+    @Test
+    @DisplayName("토큰을 통해 계정 정보를 가져올 수 있다")
+    void getAccountContextFromToken() throws Exception {
+        // given
+        AccountDetails accountDetails = createAccountDetails(1L, "testname1");
+        String token = jwtProvider.generateToken(accountDetails);
+
+        // when
+        AccountContext accountContext = jwtProvider.getAccountContextFromToken(token);
+
+        // then
+        assertThat(accountContext.accountId()).isEqualTo(1L);
+        assertThat(accountContext.authorities()).isEqualTo(accountDetails.getAuthorities());
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 토큰으로 계정정보를 가지고 올 수 없다")
+    void getAccountContextFromToken_whenInvalidToken() throws Exception {
+        // given
+        String token = "invalidvalidfioewfsdfdfsf";
+
+        // when & then
+        assertThatThrownBy(() -> jwtProvider.getAccountContextFromToken(token))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Invalid Token: " + token);
+    }
+
+    private AccountDetails createAccountDetails(Long accountId, String username) {
+        AccountDto accountDto = new AccountDto(accountId, username, "password1");
+        return new AccountDetails(accountDto, List.of(new SimpleGrantedAuthority("ROLE_USER")));
+    }
+}
